87770171bdc446c1fae060e6cb26cd769d29b25d
The Jedi <TheJedi@example.com>
2021-06-02 20:50:54 +0200

    Adding script to install vscode extensions

---
+++ scripts/install-vscode-extensions.sh
@@ -0,0 +1,7 @@
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension elmTooling.elm-ls-vscode
+code --install-extension esbenp.prettier-vscode
+code --install-extension heybourn.headwind
+code --install-extension Orta.vscode-jest
+code --install-extension PKief.material-icon-theme
