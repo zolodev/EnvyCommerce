b21ebfaa26b92efef10a531020e40935feead15c
The Jedi <TheJedi@example.com>
2021-06-06 16:37:46 +0200

    Adding Enzyme and a few test cases for TDD

--- test/index.test.js
+++ test/index.test.js
@@ -1,20 +1,24 @@
-import { render } from "@testing-library/react";
-import Home, { addAwesome } from "../src/pages/index";
+import { shallow } from "enzyme";
+import Home from "../src/pages/index";
+
+const wrapper = shallow(<Home />);
 
 // `describe` is not required, but it helps the tests read nicely
 describe("The Home Page Component", () => {
   // Each test for the component will get an `it` block
   it("should have exactly 1 `main` section", () => {
     // The getByRole will error if there are less or more than 1 element found
-    const { getByRole } = render(<Home />);
-    const main = getByRole("main");
-
-    expect(main).toBeInTheDocument();
+    expect(wrapper.find("main").type()).toBe("main");
   });
-});
 
-describe("Test add function", () => {
-  test("Test add function...", () => {
-    expect(addAwesome(1, 2)).toBe(3);
+  it("should have an search input", () => {
+    expect(wrapper.find("input[type='search']").type()).toBe("input");
+  });
+
+  it("the search field should have a placholder with text", () => {
+    const searchField = wrapper.find("input");
+    expect(searchField.props().placeholder).toBe(
+      "Search and filter products..."
+    );
   });
 });
